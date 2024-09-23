67f1aab954078566d3e4437681f115bc250d9849
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-23 13:38:12 -0300

    Projeto base

---
+++ backend/arqsoftware/src/main/java/com/ucsal/arqsoftware/ArqsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.ucsal.arqsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqsoftwareApplication.class, args);
+	}
+
+}
